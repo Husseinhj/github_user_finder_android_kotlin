7fb1133db3ccec757d32d887daec16810dc6a496
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-03-05 14:31:17 +0330

    Create Github-API models for user search and details

---
+++ app/src/main/java/com/github/husseinhj/githubuser/models/data/UserDetailsResponseModel.kt
@@ -0,0 +1,25 @@
+package com.github.husseinhj.githubuser.models.data
+
+import com.google.gson.annotations.SerializedName
+
+data class UserDetailsResponseModel (
+    val name: String? = null,
+    val company: String? = null,
+    val blog: String? = null,
+    val location: String? = null,
+    val email: String? = null,
+    val hireable: Boolean? = null,
+    val bio: String? = null,
+    @SerializedName("twitter_username")
+    val twitterUsername: String? = null,
+    @SerializedName("public_repos")
+    val publicRepos: Long? = null,
+    @SerializedName("public_gists")
+    val publicGists: Long? = null,
+    val followers: Long? = null,
+    val following: Long? = null,
+    @SerializedName("created_at")
+    val createdAt: String? = null,
+    @SerializedName("updated_at")
+    val updatedAt: String? = null
+): UserSimpleDetailsModel()
---
+++ app/src/main/java/com/github/husseinhj/githubuser/models/data/UserSearchResponseModel.kt
@@ -0,0 +1,11 @@
+package com.github.husseinhj.githubuser.models.data
+
+import com.google.gson.annotations.SerializedName
+
+data class UserSearchResponseModel (
+    @SerializedName("total_count")
+    val totalCount: Long? = null,
+    @SerializedName("incomplete_results")
+    val incompleteResults: Boolean? = null,
+    val items: List<UserSimpleDetailsModel>? = null
+)
