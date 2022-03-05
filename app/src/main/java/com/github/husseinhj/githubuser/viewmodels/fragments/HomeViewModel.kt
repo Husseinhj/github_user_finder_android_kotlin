e470ef823d80ef1e4468666d4c507bded19ef098
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-03-06 00:14:54 +0330

    Refactor main activity class and remove EventBus approach To handle toolbar states use ToolbarAppearance class

--- app/src/main/java/com/github/husseinhj/githubuser/viewmodels/fragments/HomeViewModel.kt
+++ app/src/main/java/com/github/husseinhj/githubuser/viewmodels/fragments/HomeViewModel.kt
@@ -1,13 +1,5 @@
 package com.github.husseinhj.githubuser.viewmodels.fragments
 
 import androidx.lifecycle.ViewModel
-import com.github.husseinhj.githubuser.models.eventbus.OnSearchBarMessage
-import org.greenrobot.eventbus.EventBus
 
-class HomeViewModel: ViewModel() {
-
-    fun onStartButtonClicked() {
-        EventBus.getDefault().post(OnSearchBarMessage(focused = true))
-    }
-
-}
+class HomeViewModel: ViewModel()
