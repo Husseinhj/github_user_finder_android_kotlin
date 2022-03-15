0731021e7ff49a55ebf2c122559795dfa3a3dda3
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-03-15 17:13:10 +0330

    Refactor retrofit builder and define module of koin DI

--- app/src/main/java/com/github/husseinhj/githubuser/services/providers/WebServiceProvider.kt
+++ app/src/main/java/com/github/husseinhj/githubuser/services/providers/WebServiceProvider.kt
@@ -1,31 +1,39 @@
 package com.github.husseinhj.githubuser.services.providers
 
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import org.koin.core.scope.Scope
+import java.util.concurrent.TimeUnit
+import okhttp3.logging.HttpLoggingInterceptor
+import com.github.husseinhj.githubuser.BuildConfig
+import retrofit2.converter.gson.GsonConverterFactory
+import com.github.husseinhj.githubuser.consts.READ_TIMEOUT
+import com.github.husseinhj.githubuser.consts.WRITE_TIMEOUT
+import com.github.husseinhj.githubuser.consts.CONNECT_TIMEOUT
 import com.github.husseinhj.githubuser.consts.GITHUB_API_BASE_URL
 import com.github.husseinhj.githubuser.services.interceptors.AuthenticationInterceptor
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
-object WebServiceProvider {
+fun Scope.getRetrofit(): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(GITHUB_API_BASE_URL)
+        .client(get())
+        .addConverterFactory(GsonConverterFactory.create(get()))
+        .build()
+}
 
-    private fun getRetrofit(): Retrofit {
-        val okHttpClient = OkHttpClient().newBuilder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS)
-            .addInterceptor(AuthenticationInterceptor())
-            .build()
+fun Scope.retrofitHttpClient(): OkHttpClient {
+    return OkHttpClient().newBuilder()
+        .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
 
-        return Retrofit.Builder()
-            .baseUrl(GITHUB_API_BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    fun <S> createService(serviceClass: Class<S>?): S {
-        return getRetrofit().create(serviceClass!!)
-    }
-
+        .addInterceptor(get() as AuthenticationInterceptor)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.HEADERS
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        })
+        .build()
 }
