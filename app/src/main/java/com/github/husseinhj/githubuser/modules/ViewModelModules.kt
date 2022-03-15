a82d351fbce64c92cbf04d5a22f1d2b80a8998ff
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-03-15 17:08:26 +0330

    Add inject HomeViewModel into the HomeFragment class

--- app/src/main/java/com/github/husseinhj/githubuser/modules/ViewModelModules.kt
+++ app/src/main/java/com/github/husseinhj/githubuser/modules/ViewModelModules.kt
@@ -2,11 +2,17 @@ package com.github.husseinhj.githubuser.modules
 
 import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
+import com.github.husseinhj.githubuser.viewmodels.fragments.HomeViewModel
 import com.github.husseinhj.githubuser.viewmodels.activities.MainActivityViewModel
 
 val viewModelModules = module {
     // Main Activity ViewModel
     viewModel {
         MainActivityViewModel()
+    }
+
+    // On-Boarding ViewModel
+    viewModel {
+        HomeViewModel()
     }
 }
